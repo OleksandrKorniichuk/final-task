bd0c5710d04c3323af4b7c5f0338f82cd2c18442
Oleksandr Korniichuk <kov0249@gmail.com>
2014-09-01 00:43:07 +0300

    slide 2 5%

--- scripts/tab-slider.js
+++ scripts/tab-slider.js
@@ -1,11 +1,9 @@
 /*Меню*/
 (function(){
 	"use strict";
-
 	var tabs = document.getElementsByClassName("nav-tab-item"),
 		pages = document.getElementsByClassName("page"),
 		current = 0;
-
 	[].forEach.call(tabs, function(tab, index){
 		tab.addEventListener("click", function(event){
 			tabs[current].classList.remove("nav-tab-item-active");
@@ -19,17 +17,16 @@
 
 /*Анимация 3 слайда*/
 function libra(){
-	var leftCup = document.getElementsByClassName('competitors-scales-block'),
-		rightCup = document.getElementsByClassName('competitors-scales-block'),
-		libra = document.getElementsByClassName('competitors-vertical-hr');
-
-	leftCup[0].addEventListener('click', function(event){
-		libra[0].classList.add('left-win');
-		libra[0].classList.remove('left-win');
+	var RotateLeft = document.getElementsByClassName('competitors-scales-right-top-link'),
+		RotateRight = document.getElementsByClassName('competitors-scales-right-bottom-link'),
+		ScalesBlock = document.getElementsByClassName('competitors-scales-block');
+	RotateLeft[0].addEventListener('click', function(event){
+		ScalesBlock[0].classList.add('left-win');
+		ScalesBlock[0].classList.remove('right-win');
 	});
-	rightCup[0].addEventListener('click', function(event){
-		libra[0].classList.add('left-win');
-		libra[0].classList.remove('left-win');
+	RotateRight[0].addEventListener('click', function(event){
+		ScalesBlock[0].classList.add('right-win');
+		ScalesBlock[0].classList.remove('left-win');
 	});
 
 };
